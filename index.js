4afaddcee706b46ca14e6bb3d7f2826345dd2bbc
Kyle Corbitt <kyle@corbt.com>
2016-06-25 11:48:43 -0600

    remove opaque counter

--- index.js
+++ index.js
@@ -3,21 +3,13 @@
 import React, { Component } from 'react';
 import { NativeModules } from 'react-native';
 
-// Track the number of calls to `activate` and `deactivate`. Only disable the wake lock if the calls
-// to `deactivate` equal or exceed the calls to `activate`. This enables nesting.
-let usageCount = 0;
-
 export default class KeepAwake extends Component {
   static activate() {
-    usageCount += 1;
     NativeModules.KCKeepAwake.activate();
   }
 
   static deactivate() {
-    usageCount -= 1;
-    if (usageCount <= 0) {
-      NativeModules.KCKeepAwake.deactivate();
-    }
+    NativeModules.KCKeepAwake.deactivate();
   }
 
   componentWillMount() {
