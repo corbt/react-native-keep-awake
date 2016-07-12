08817c11b674b9843fefb7277f96789e9ac8442f
Avishay Bassa <avishay@geekmedia.co.il>
2016-07-12 19:44:32 +0300

    - Fixed a crash while accidentally calling activate() or deactivate() when these conditions are already met.

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ import React, { Component } from 'react';
 import { NativeModules } from 'react-native';
 
 export default class KeepAwake extends Component {
+
+  static isActive = false;
+
   static activate() {
-    NativeModules.KCKeepAwake.activate();
+      if (!KeepAwake.isActive) {
+          NativeModules.KCKeepAwake.activate();
+          KeepAwake.isActive = true;
+      }
   }
 
   static deactivate() {
-    NativeModules.KCKeepAwake.deactivate();
+      if (!KeepAwake.isActive) {
+          NativeModules.KCKeepAwake.deactivate();
+          KeepAwake.isActive = false;      
+      }
   }
 
   componentWillMount() {
