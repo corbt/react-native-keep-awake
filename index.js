dbf91b468a8f00f34373f6186ad8ebe0c8cb7c3a
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-02-17 14:48:32 +0200

    Remove active state from JS Native method can be called even though sleep preventing flag are set. There are no drawbacks to this

--- index.js
+++ index.js
@@ -4,21 +4,12 @@ import React, { Component } from 'react';
 import { NativeModules } from 'react-native';
 
 export default class KeepAwake extends Component {
-
-  static isActive = false;
-
   static activate() {
-      if (!KeepAwake.isActive) {
-          NativeModules.KCKeepAwake.activate();
-          KeepAwake.isActive = true;
-      }
+    NativeModules.KCKeepAwake.activate();
   }
 
   static deactivate() {
-      if (KeepAwake.isActive) {
-          NativeModules.KCKeepAwake.deactivate();
-          KeepAwake.isActive = false;      
-      }
+    NativeModules.KCKeepAwake.deactivate();
   }
 
   componentWillMount() {
