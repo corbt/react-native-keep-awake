5daeb4f8fdaf485b9c4996409c918ecfc6915bfd
Peace <peacechen@users.noreply.github.com>
2017-06-19 12:11:24 -0500

    [iOS] Support RN 0.4x as well as older <= 0.3x
    See #27

--- ios/KCKeepAwake.h
+++ ios/KCKeepAwake.h
@@ -1,4 +1,10 @@
+#if __has_include(<React/RCTBridgeModule.h>)
+// React Native >= 0.40
 #import <React/RCTBridgeModule.h>
-
+else
+// React Native <= 0.39
+#import "RCTBridgeModule.h"
+#endif
+  
 @interface KCKeepAwake : NSObject <RCTBridgeModule>
 @end
